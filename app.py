76c990584f1f2fdc3e9aacd9f734a3aa5a292ef3
benActor <210335@university-365.com>
2021-03-24 15:03:04 +0100

    optimisation based on load implemented

--- app.py
+++ app.py
@@ -19,66 +19,11 @@ TODO
 
 4 - get load higher or equal to target value
 
-5 - choose the optimal load based on cost 
+5 - choose the optimal load based on load / cost
 
-
-{
-  "load": 910,
-  "fuels":
-  {
-    "gas(euro/MWh)": 13.4,
-    "kerosine(euro/MWh)": 50.8,
-    "co2(euro/ton)": 20,
-    "wind(%)": 60
-  },
-  "powerplants": [
-    {
-      "name": "gasfiredbig1",
-      "type": "gasfired",
-      "efficiency": 0.53,
-      "pmin": 100,
-      "pmax": 460
-    },
-    {
-      "name": "gasfiredbig2",
-      "type": "gasfired",
-      "efficiency": 0.53,
-      "pmin": 100,
-      "pmax": 460
-    },
-    {
-      "name": "gasfiredsomewhatsmaller",
-      "type": "gasfired",
-      "efficiency": 0.37,
-      "pmin": 40,
-      "pmax": 210
-    },
-    {
-      "name": "tj1",
-      "type": "turbojet",
-      "efficiency": 0.3,
-      "pmin": 0,
-      "pmax": 16
-    },
-    {
-      "name": "windpark1",
-      "type": "windturbine",
-      "efficiency": 1,
-      "pmin": 0,
-      "pmax": 150
-    },
-    {
-      "name": "windpark2",
-      "type": "windturbine",
-      "efficiency": 1,
-      "pmin": 0,
-      "pmax": 36
-    }
-  ]
-}
 
 """
-@app.route('/productionplan', methods=["GET", "POST"])
+@app.route('/productionplan', methods=["POST"])
 def production_plan():
     types = {
         "one": "gasfired",
@@ -108,12 +53,14 @@ def production_plan():
             matching_comb = [comb for comb in all_combinations if
                              (pwp_helper.total_pmax_pwp_set(pwp_set=comb, fuels_json=fuels, types=types) >= load)]
 
-            optimal_composition = max(matching_comb, key=lambda x: pwp_helper.pmax_over_cost(pwp_set=x, fuels_json=fuels, types=types))
+
+            # here we consider the most optimal based on load
+            optimal_composition = min(matching_comb, key=lambda x: pwp_helper.total_pmax_pwp_set(pwp_set=x, fuels_json=fuels, types=types))
 
             result = [{"name" : x["name"], "p": 0} for x in power_plants if x not in optimal_composition] + \
                      [{"name": y["name"], "p": y["pmax"]} for y in optimal_composition]
             return jsonify(result)
-    return 'hoho'
+    return {}
 
 
 
